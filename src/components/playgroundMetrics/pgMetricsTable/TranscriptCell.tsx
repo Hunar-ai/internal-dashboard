f76fdf3a8e03da1c64a3b68061ada66c48571373
Nithish Naidu <nithish@hunar.ai>
2025-03-20 15:56:57 +0530

    Refactor :hammer: Extract components to separate files

--- src/components/playgroundMetrics/pgMetricsTable/TranscriptCell.tsx
+++ src/components/playgroundMetrics/pgMetricsTable/TranscriptCell.tsx
@@ -1,31 +1,17 @@
 import { ModalWrapper, type Cell } from 'components/common';
 
-import { ChatBubble, type ChatBubbleProps } from './ChatBubble';
-
-interface ChatProps {
-    transcript: ChatBubbleProps[];
-}
+import { ChatView } from './ChatView';
 
 interface TranscriptCellProps {
     cell: Cell;
 }
-
-const Chat = ({ transcript = [] }: ChatProps) => {
-    return (
-        <>
-            {transcript.map((msg, index) => (
-                <ChatBubble key={index} text={msg.text} speaker={msg.speaker} />
-            ))}
-        </>
-    );
-};
 
 export const TranscriptCell = ({ cell }: TranscriptCellProps) => {
     if (!cell?.value?.length) return <></>;
 
     return (
         <ModalWrapper CTA="View Conversation" title={`Conversations`}>
-            <Chat transcript={cell?.value} />
+            <ChatView transcript={cell?.value} />
         </ModalWrapper>
     );
 };
